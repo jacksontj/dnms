jacksontj/dnms

commit 64cc26927b93822d7837f78ade425cde6d76d7bf

Change `route` to not include the src/dst peers in the route-- this allows us to share pings across the same route across peers. We don't actually end up losing data as the mapper/routemap knows which src/dst pair gets you to which route

mapper/mapper.go
@@ -82,6 +82,7 @@ func (m *Mapper) RemovePeer(p Peer) {
 	}
 }
 
+// TODO: randomize shuffle (since this is used for mapping and pinging
 // TODO: better, since this will be concurrent
 func (m *Mapper) IterPeers() chan *Peer {
 	peerChan := make(chan *Peer)
@@ -173,7 +174,12 @@ func (m *Mapper) mapPeer(p *Peer, srcPort int) {
 		_, ok := m.peerMap[p.Name]
 		if ok {
 			// Add new one
-			newRoute, _ := m.Graph.IncrRoute(path, nil)
+			// TODO: remove? for now we'll try storing only the "middle" parts of the
+			// route, since our goal is to map the network-- not so much the peers
+			//logrus.Infof("path: %v", path)
+			//newRoute, _ := m.Graph.IncrRoute(path, nil)
+			logrus.Infof("path: %v", path[1:len(path)-1])
+			newRoute, _ := m.Graph.IncrRoute(path[1:len(path)-1], nil)
 			m.RouteMap.UpdateRouteOption(m.localName, srcPort, p.String(), newRoute)
 
 			// Remove old one if it exists

TODO
@@ -8,6 +8,7 @@ TODO:
         -- sharding boolean
         -- Saggregator (super aggregator)
         -- weighted hash to aggregator based on # of peers
+        -- aggregate mapper/routemap
     - memberlist: use the meta field
         -- include "role"
         -- include http endpoint info (port at least)?
@@ -21,7 +22,6 @@ TODO:
             -- check ordering
     - separate the traceroute lib into a CLI + lib
     - Cleanup state serialization -- preferrably a string instead of a uint8 (up/suspect/down instead of 0/1/2)
-    - change "route" to not include src/dst nodes (to reduce redundant routes).
 
 In the future:
     - support TCP pings

mapper/routemap.go
@@ -153,6 +153,11 @@ func (r *RouteMap) MarshalJSON() ([]byte, error) {
 	r.lock.RLock()
 	defer r.lock.RUnlock()
 
-	// just return the map (the rest are really just indexes)
-	return json.Marshal(r.NodeRouteMap)
+	// TODO: iteratively create this-- making copies is a bit lame :/
+	// return map of key -> route.Key()
+	tmp := make(map[string]string)
+	for k, route := range r.NodeRouteMap {
+		tmp[k] = route.Key()
+	}
+	return json.Marshal(tmp)
 }

commit 50e8efb47d5ce5868e51ae597f8692a83bbf9030

Fix bug introduced in 64cc26927b93822d7837f78ade425cde6d76d7bf
When removing the source and dst nodes from the path, it was done after the check that the routes where the same. This mean that routes where always different. In addition-- the first hop in the network is interesting, as it isn't the peer. The goal here is to not have a peer address in the "path"

mapper/mapper.go
@@ -237,6 +237,12 @@ func (m *Mapper) mapPeer(p *Peer, srcPort int) {
 		}
 
 	}
+	// strip out first and last-- this makes the graph more connected, since we
+	// aren't really interested in mapping peers-- so much as the network in the
+	// middle. We don't lose data, because the RouteMap	keeps track of which peers
+	// send down which routes
+	// TLDR; the goal is to not have a peer in a `path`
+	path = path[:len(path)-1]
 	logrus.Debugf("traceroute path: %v", path)
 
 	currRoute := m.RouteMap.GetRouteOption(m.localName, srcPort, p.Name, p.Port)
@@ -248,7 +254,7 @@ func (m *Mapper) mapPeer(p *Peer, srcPort int) {
 		_, ok := m.peerMap[p.Name]
 		if ok {
 			// Add new one
-			newRoute, _ := m.Graph.IncrRoute(path[1:len(path)-1], nil)
+			newRoute, _ := m.Graph.IncrRoute(path, nil)
 			m.RouteMap.UpdateRouteOption(m.localName, srcPort, p.String(), newRoute)
 
 			// Remove old one if it exists
@@ -257,10 +263,10 @@ func (m *Mapper) mapPeer(p *Peer, srcPort int) {
 				// because something returned "*") then lets keep the old one
 				// for some period of time
 				logrus.Infof("replaced path old: %v", currRoute.Hops())
-				logrus.Infof("replaced path new: %v", path[1:len(path)-1])
+				logrus.Infof("replaced path new: %v", path)
 				m.Graph.DecrRoute(currRoute.Hops())
 			} else {
-				logrus.Infof("new path: %v", path[1:len(path)-1])
+				logrus.Infof("new path: %v", path)
 			}
 		}
 		m.peerLock.RUnlock()